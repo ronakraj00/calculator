c237f649a9389eacac4f491cf6c4401d3bdab0fb
ronakraj00 <12213052@nitkkr.ac.in>
2023-05-06 15:24:24 +0530

    add Javascript

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+let exp;
 //function for button control
 const buttons=document.querySelectorAll(".button");
-buttons.forEach(button,())
+const display=document.querySelector("#numbers")
+buttons.forEach(button=>{
+    button.addEventListener("click",(evt)=>{
+        if(evt.target.id==''){
+            display.innerText="";
+        }
+        else if(evt.target.id=='='){
+            console.log(exp);
+            display.innerText=eval(exp);
+        }
+        else{
+            display.innerText+=evt.target.id;
+            exp=display.innerHTML;   
+        }
+        
+    })
+});
+
+
 
